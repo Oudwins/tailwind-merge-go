55c5402d3df88fd01e946846be508d20566c6189
tristan <tm@tristanmayo.com>
2024-03-12 08:20:28 +0100

    feat: improved caching by using two mutexes

--- pkg/cache/lru.go
+++ pkg/cache/lru.go
@@ -20,39 +20,45 @@ type LRU struct {
 	cache       map[string]*node
 	head        *node
 	tail        *node
-	mutex       sync.Mutex
+	cacheMutex  sync.RWMutex
+	listMutex   sync.Mutex
 }
 
 func (lru *LRU) Get(key string) string {
-	lru.mutex.Lock()
-	defer lru.mutex.Unlock()
+	lru.cacheMutex.RLock()
 	n := lru.cache[key]
 	if n == nil {
 		return ""
 	}
+	lru.cacheMutex.RUnlock()
 
+	lru.listMutex.Lock()
 	lru.remove(n)
 	lru.insertRight(n)
+	lru.listMutex.Unlock()
 
 	return n.val
 }
 
 func (lru *LRU) Set(key, value string) {
-	lru.mutex.Lock()
-	defer lru.mutex.Unlock()
+	lru.cacheMutex.Lock()
 	if n := lru.cache[key]; n != nil {
 		lru.remove(n)
 	}
-
 	n := &node{key: key, val: value}
 	lru.cache[key] = n
+	lru.cacheMutex.Unlock()
+	lru.listMutex.Lock()
 	lru.insertRight(n)
-
+	lru.listMutex.Unlock()
 	// evict
+
+	lru.listMutex.Lock()
 	if lru.capacity > lru.maxCapacity {
 		delete(lru.cache, lru.tail.next.key)
 		lru.remove(lru.tail.next)
 	}
+	lru.listMutex.Unlock()
 
 }
 
